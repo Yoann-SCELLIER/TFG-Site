ebe2b5fb157dfd22fae71520b1cbd1f671d93999
Yoann <scellier.yoann@gmail.com>
2024-05-26 00:11:01 +0200

    Modification version 1

---
+++ assets/js/javascript.js
@@ -0,0 +1,59 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    // STORY -----------------------------------------------------------------------------------------------------------------------
+    document.getElementById('showMoreStory').addEventListener('click', function () {
+        // Afficher le contenu complet de l'histoire
+        document.getElementById('hiddenStoryContent').style.display = 'block';
+        // Cacher la vue précédente
+        document.querySelector('.downStoryContent').style.display = 'none';
+        // Cacher le bouton "Voir plus"
+        document.getElementById('showMoreStory').style.display = 'none'; 
+    });
+
+    document.getElementById('showLessStory').addEventListener('click', function () {
+        // Cacher le contenu complet de l'histoire
+        document.getElementById('hiddenStoryContent').style.display = 'none';
+        // Afficher la vue précédente
+        document.querySelector('.downStoryContent').style.display = 'block';
+        // Afficher le bouton "Voir plus"
+        document.getElementById('showMoreStory').style.display = 'inline';
+    });
+
+    // MEMBRE ET STAFF -----------------------------------------------------------------------------------------------------------------------
+    document.getElementById('showMoreStaff').addEventListener('click', function () {
+        // Afficher le contenu complet de l'histoire
+        document.getElementById('hiddenStaff').style.display = 'block';
+        // Cacher la vue précédente
+        document.querySelector('.downStaff').style.display = 'none';
+        // Cacher le bouton "Voir plus"
+        document.getElementById('showMoreStaff').style.display = 'none'; 
+    });
+
+    document.getElementById('showLessStaff').addEventListener('click', function () {
+        // Cacher le contenu complet de l'histoire
+        document.getElementById('hiddenStaff').style.display = 'none';
+        // Afficher la vue précédente
+        document.querySelector('.downStaff').style.display = 'block';
+        // Afficher le bouton "Voir plus"
+        document.getElementById('showMoreStaff').style.display = 'inline';
+    });
+
+    // GAMING -----------------------------------------------------------------------------------------------------------------------
+    document.getElementById('showMoreGaming').addEventListener('click', function () {
+        // Afficher le contenu complet de gaming
+        document.getElementById('hiddenGamingContent').style.display = 'block';
+        // Cacher la vue précédente
+        document.querySelector('.downGamingContent').style.display = 'none';
+        // Cacher le bouton "Voir plus"
+        document.getElementById('showMoreGaming').style.display = 'none';
+    });
+
+    document.getElementById('showLessGaming').addEventListener('click', function () {
+        // Cacher le contenu complet de gaming
+        document.getElementById('hiddenGamingContent').style.display = 'none';
+        // Afficher la vue précédente
+        document.querySelector('.downGamingContent').style.display = 'block';
+        // Afficher le bouton "Voir plus"
+        document.getElementById('showMoreGaming').style.display = 'inline';
+    });
+});
