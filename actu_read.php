0a2bc533d77f056b510204e46b606a595511e9ed
Yoann <scellier.yoann@gmail.com>
2024-06-06 09:26:48 +0200

    MAJ redirection global des pages index

--- actu_read.php
+++ actu_read.php
@@ -1,7 +1,11 @@
 <?php
-require_once dirname(__DIR__) . '\TFG\controller\db.fn.php';
 require_once dirname(__DIR__) . '\TFG\components\header.html.php';
+
 require_once dirname(__DIR__) . '\TFG\controllers\views_actu.php';
+
 require_once dirname(__DIR__) . '\TFG\views\actu_reade.html.php';
+
+require_once dirname(__DIR__) . '\TFG\views\formulaire.html.php';
+
 require_once dirname(__DIR__) . '\TFG\components\footer.html.php';
 ?>
