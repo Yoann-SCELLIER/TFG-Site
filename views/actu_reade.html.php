892a0f3d2bbf34dffb619ec28c2801d6f9d5e9d6
Yoann <scellier.yoann@gmail.com>
2024-06-10 21:38:28 +0200

    MAJ button actu update / delete séparation

--- views/actu_reade.html.php
+++ views/actu_reade.html.php
@@ -25,17 +25,6 @@ if ($post) {
                 <p>Modifié le : <small class="text-body-secondary"><?= $post['modif_at'] ?></small></p>
             <?php endif; ?>
         </div>
-        <div aria-label="Actions" class="text-center m-3">
-            <a href="actu_formu.php?id=<?= $post['post_id'] ?>" class="btn btn-warning">Modifier l'article</a>
-        </div>
-        <?php if ($id) : ?>
-            <div aria-label="Actions" class="text-center m-3">
-                <form action="/TFG/controllers/delete_post.php" method="post" style="display: inline;">
-                    <input type="hidden" name="id" value="<?= htmlspecialchars($id) ?>">
-                    <button type="submit" class="btn btn-danger">Supprimer l'article</button>
-                </form>
-            </div>
-        <?php endif; ?>
     </div>
 </div>
 
