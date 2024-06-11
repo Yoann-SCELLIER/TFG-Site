b51c4d773d943db865720f78d38501562976cdad
Yoann <scellier.yoann@gmail.com>
2024-06-11 09:45:05 +0200

    MAJ responsive gaming

--- views/gaming.html.php
+++ views/gaming.html.php
@@ -20,56 +20,73 @@
 <!-- CONTENU CACHÉ -->
 <div id="hiddenGamingContent" class="text-center" style="display: none;">
 
-    <div class="row row-cols-1 row-cols-md-2 text-center g-0 p-1">
-        <div class="col">
-            <img class="p-0 m-0 b-0 g-0" width="40%" height="auto" src="assets/images/img-gaming.png" alt="image pour la section gaming">
+    <div class="text-center">
+        <div class="row row-cols-1 row-cols-md-2 text-center g-0 p-1" background-size="10%">
+            <div class="col align-self-center text-end order-2 order-sm-2 order-md-2 order-lg-2">
+                <div class="col row align-self-center text-start m-0">
+                    <h1 class="fs-4">Gaming</h1>
+                    <p class="fs-6">Liste des jeux disponibles sur les <br>
+                        plateformes précises où vous pouvez nous rejoindre.</p>
+                </div>
+            </div>
+            <div class="col g-0 p-0 m-0 b-0 order-1 order-sm-1 order-md-1 order-lg-1">
+                <img class="p-0 m-0 b-0 g-0" width="40%" height="auto" src="assets/images/img-gaming.png" alt="image pour la section gaming">
+            </div>
         </div>
-        <div class="col row align-self-center text-start m-0">
-            <h1 class="fs-4">Gaming</h1>
-            <p class="fs-6">Liste des jeux disponibles sur les <br>
-                plateformes précises où vous pouvez nous rejoindre.</p>
-        </div>
-    </div>
-    <hr class="border-1 text-dark">
 
-    <div class="container">
-        <div class="row d-flex justify-content-evenly">
-            <?php foreach ($games as $game) : ?>
-                <div class="col-md-4 d-flex flex-column align-items-center" style="width: 280px;">
-                    <!-- Afficher l'image du membre -->
-                    <div style="width: 60%; height: auto;">
-                        <img src="<?= htmlspecialchars($game['cover']) ?>" class="card-img-top h-auto" alt="<?= htmlspecialchars($game['title']) ?>">
-                    </div>
-                    <div class="card mb-4 border border-0">
-                        <div class="card-body p-0 m-0 b-0 g-0">
-                            <h5 class="card-title fs-6"><?= htmlspecialchars($game['title']) ?></h5>
+
+
+    <!-- <div class="row row-cols-1 row-cols-md-2 text-center g-0 p-1">
+            <div class="col">
+                <img class="p-0 m-0 b-0 g-0" width="40%" height="auto" src="assets/images/img-story.png" alt="image pour la section story">
+            </div>
+            <div class="col row align-self-center text-start m-0">
+                <h1 class="fs-4">Histoire de la True Fighters Gaming</h1>
+                <p class="fs-6">Découvrez l'histoire de la True Fighters Gaming (TF), ainsi que ses débuts, sa
+                    vision de
+                    l'univers du gaming et ses objectifs communs.</p>
+            </div>
+        </div> -->
+        <hr class="border-1 text-dark">
+
+        <div class="container">
+            <div class="row d-flex justify-content-evenly">
+                <?php foreach ($games as $game) : ?>
+                    <div class="col-md-4 d-flex flex-column align-items-center" style="width: 280px;">
+                        <!-- Afficher l'image du membre -->
+                        <div style="width: 60%; height: auto;">
+                            <img src="<?= htmlspecialchars($game['cover']) ?>" class="card-img-top h-auto" alt="<?= htmlspecialchars($game['title']) ?>">
+                        </div>
+                        <div class="card mb-4 border border-0">
+                            <div class="card-body p-0 m-0 b-0 g-0">
+                                <h5 class="card-title fs-6"><?= htmlspecialchars($game['title']) ?></h5>
+                            </div>
                         </div>
                     </div>
-                </div>
-            <?php endforeach; ?>
-        </div>
-    </div>
-</div>
-
-<div class="text-center bg-grey text-white">
-    <h1>Esport</h1>
-    <p>Les tournois proposés par la True Fighters (TF).</p>
-    <hr class="border-1 text-light">
-    <div class="container">
-        <div class="row d-flex justify-content-evenly">
-            <div class="col-md-4 d-flex flex-column align-items-center" style="width: 400px;">
-                <img src="assets\images\logo-tournament.png" class="card-img-top h-auto" alt="Logo tournois Guilty Gear -STRIVE-">
-                <div class="card mb-4 border border-0 bg-transparent">
-                    <div class="card-body bg-transparent p-0 m-0 b-0 g-0">
-                        <h5 class="card-title fs-6 text-white">Tournament Guilty Gear -STRIVE- True Fighters</h5>
-                    </div>
-                </div>
+                <?php endforeach; ?>
             </div>
         </div>
     </div>
 
-    <div class="p-2">
-        <i class="bi bi-chevron-double-up" id="showLessGaming">VOIR MOINS...</i> <!-- BOUTON QUI CACHE LE CONTENU ET AFFICHE LA PREMIÈRE PARTIE -->
+    <div class="text-center bg-grey text-white">
+        <h1>Esport</h1>
+        <p>Les tournois proposés par la True Fighters (TF).</p>
+        <hr class="border-1 text-light">
+        <div class="container">
+            <div class="row d-flex justify-content-evenly">
+                <div class="col-md-4 d-flex flex-column align-items-center" style="width: 400px;">
+                    <img src="assets\images\logo-tournament.png" class="card-img-top h-auto" alt="Logo tournois Guilty Gear -STRIVE-">
+                    <div class="card mb-4 border border-0 bg-transparent">
+                        <div class="card-body bg-transparent p-0 m-0 b-0 g-0">
+                            <h5 class="card-title fs-6 text-white">Tournament Guilty Gear -STRIVE- True Fighters</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="p-2">
+            <i class="bi bi-chevron-double-up" id="showLessGaming">VOIR MOINS...</i> <!-- BOUTON QUI CACHE LE CONTENU ET AFFICHE LA PREMIÈRE PARTIE -->
+        </div>
     </div>
-</div>
 </div>
